f1e9e55d353655636c56b5450f2a23172321c13e
eduardo.anjos <eduardo.h.anjos0411@gmail.com>
2025-07-18 12:25:27 -0300

    refactor: adding GovBRRadioProps types to proper file

---
+++ src/components/GovBRRadio/types.ts
@@ -0,0 +1,17 @@
+import { SharedUIComponentProps } from "src/types/SharedUIComponentProps";
+
+/**
+ * @property id - Define o identificador do componente
+ * @property label - Texto exibido no topo do componente
+ * @property helpText - Texto  para informações adicionais
+ * @property text - Texto da opção 
+ * @property status - Define se o campo de radio é válido ou inválido
+ */
+
+export interface GovBRRadioProps extends SharedUIComponentProps {
+  id?: string
+  label?: string
+  helpText?: string
+  text?: string
+  status?: 'valid' | 'invalid'
+}
