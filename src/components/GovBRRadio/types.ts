865e8bc05a9f82eb6fe0c23c3942f357f29db4ee
Eduardo Henrique <eduardo.h.anjos0411@gmail.com>
2025-07-22 15:17:06 -0300

    28 adicionar componente govbrradio (#45)
    * refactor: Rebuilding component using MUI
    
    * style: remobing unnecessary imports to match lint
    
    * refactor: Changing component with a different approach on standalone Radio
    
    * refactor: Improve GovBRRadio component structure and styling in stories
    
    ---------
    
    Co-authored-by: Luciano Édipo <luciano.psilva@anpd.gov.br>

--- src/components/GovBRRadio/types.ts
+++ src/components/GovBRRadio/types.ts
@@ -1,23 +1,26 @@
-import { InputHTMLAttributes } from 'react'
+import { ButtonProps } from '@mui/material'
 import { SharedUIComponentProps } from 'src/types/SharedUIComponentProps'
 
 /**
- * @property id - Define o identificador do componente
+ * @propery helpText - Texto utilizado para informações adicionais
+ * @property text - Texto exibido na opção de seleção
  * @property label - Texto exibido no topo do componente
- * @property helpText - Texto  para informações adicionais
- * @property text - Texto da opção
  * @property disabled - Altera o estado do botão
- * @property status - Define se o campo de radio é válido ou inválido
+ * @property checked - Altera o estado de seleção
+ * @property error - Utilizado para demonstrar o estado de erro
+ * @property valid - Utilizado para demonstrar o estado válido
+ * @property onChange - função para alterar o estado da opção
  */
 
-export interface GovBRRadioProps
-  extends SharedUIComponentProps,
-    InputHTMLAttributes<HTMLInputElement> {
-  id?: string
+export interface GovBRRadioProps extends SharedUIComponentProps, ButtonProps {
+  name?: string
+  value: string
   label?: string
   helpText?: string
   text?: string
   disabled?: boolean | undefined
-  status?: 'valid' | 'invalid'
   checked?: boolean | undefined
+  error?: boolean
+  valid?: boolean
+  onChange?: () => void
 }
