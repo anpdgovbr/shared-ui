1ffaae05bb0cdbed2efac01bc1293b36eb0c9aef
eduardo.anjos <eduardo.h.anjos0411@gmail.com>
2025-07-25 13:48:11 -0300

    feat: create DateTimePicker  component

---
+++ src/components/GovBRDateTimePicker/types.ts
@@ -0,0 +1,28 @@
+import { TextFieldProps } from "@mui/material"
+import { Dayjs } from "dayjs"
+import { SharedUIComponentProps } from "src/types/SharedUIComponentProps"
+
+
+export interface GovBRDateTimePickerProps extends SharedUIComponentProps, Omit<TextFieldProps, 'label' | 'onChange'> {
+  /**
+   * Valor do campo de data e hora.
+   * Deve ser um objeto Dayjs.
+   */
+  value?: Dayjs | null
+
+  /**
+   * Função chamada quando o valor do campo é alterado.
+   */
+  onChange?: (newValue: Dayjs | null) => void
+  /**
+   * Se true, o campo será desabilitado.
+   * @default false
+   */
+  disabled?: boolean
+
+  /**
+   * Se true, o campo será somente leitura.
+   * @default false
+   */
+  readOnly: boolean
+}
