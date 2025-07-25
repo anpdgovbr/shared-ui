80c4a47833080b6f94d7c435466a3b3525a6e4ea
Luciano Édipo <luciano.psilva@anpd.gov.br>
2025-07-25 18:18:11 -0300

    refactor: update type definitions and improve code consistency across components
    - Refactored GovBRFormCheckbox and GovBRFormInput type definitions for clarity and consistency.
    - Removed unused story type definitions for GovBRFormCheckbox and GovBRFormInput.
    - Updated imports and exports in various components to follow consistent formatting.
    - Enhanced type definitions in GovBRRadio and GovBRSignIn components for better readability.
    - Cleaned up icon mapping and theme provider type definitions.
    - Adjusted the Vite configuration to reflect the correct library name.

--- types/src/components/GovBRRadio/govBRRadio.stories.d.ts
+++
@@ -1,9 +0,0 @@
-import type { Meta, StoryObj } from '@storybook/react'
-import { GovBRRadio } from './GovBRRadio.js'
-declare const meta: Meta<typeof GovBRRadio>
-export default meta
-type Story = StoryObj<typeof GovBRRadio>
-export declare const Default: Story
-export declare const States: Story
-export declare const Horizontal: Story
-//# sourceMappingURL=govBRRadio.stories.d.ts.map
--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
-      name: 'AnpdSharedUI',
+      name: '@anpdgovbr/shared-ui',
       formats: ['es', 'cjs'],
       fileName: format => `shared-ui.${format}.js`,
     },
