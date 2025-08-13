5512dfdf99b08a2c362aa56d5ef6a05585715564
Luciano Édipo <luciano.psilva@anpd.gov.br>
2025-08-13 12:35:00 -0300

    chore: remove dist/ e types/ do versionamento (agora ignorados pelo .gitignore)

--- types/src/components/ui/govbr-breadcrumb/stories.d.ts
+++
@@ -1,9 +0,0 @@
-import type { Meta, StoryObj } from '@storybook/react'
-import { GovBRBreadcrumb } from './index'
-declare const meta: Meta<typeof GovBRBreadcrumb>
-export default meta
-type Story = StoryObj<typeof GovBRBreadcrumb>
-export declare const Default: Story
-export declare const Short: Story
-export declare const Long: Story
-//# sourceMappingURL=stories.d.ts.map
--- types/src/components/ui/govbr-form-checkbox/index.d.ts
+++
@@ -1,7 +0,0 @@
-import type { FieldPath, FieldValues } from 'react-hook-form';
-import type { GovBRFormCheckboxProps } from './types';
-export declare function GovBRFormCheckbox<TFieldValues extends FieldValues, TName extends FieldPath<TFieldValues>>({ name, control, rules, required, ...govBRCheckboxProps }: Readonly<GovBRFormCheckboxProps<TFieldValues, TName>>): import("react/jsx-runtime").JSX.Element;
-export declare namespace GovBRFormCheckbox {
-    var displayName: string;
-}
-//# sourceMappingURL=index.d.ts.map
