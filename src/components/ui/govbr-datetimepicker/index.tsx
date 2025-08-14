3cc15d8e003d7432dbd60483d56b264ce075e033
Eduardo Anjos <eduardo.anjos@anpd.gov.br>
2025-08-14 08:10:47 -0300

    fix: update @mui/x-date-pickers to version 8.10.0 and add slotProps for label color customization

--- src/components/ui/govbr-datetimepicker/index.tsx
+++ src/components/ui/govbr-datetimepicker/index.tsx
@@ -1,15 +1,11 @@
-import { LocalizationProvider } from "@mui/x-date-pickers"
+import { LocalizationProvider } from '@mui/x-date-pickers'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
-import dayjs, { Dayjs } from "dayjs"
+import dayjs, { Dayjs } from 'dayjs'
 import 'dayjs/locale/pt-br'
-import React from "react"
-import { GovBRDateTimePickerProps, pickerMap } from "src/components/ui/govbr-datetimepicker/types"
+import React from 'react'
+import { GovBRDateTimePickerProps, pickerMap } from 'src/components/ui/govbr-datetimepicker/types'
 
-export function GovBRDateTimePicker ({
-  type,
-  label,
-}: Readonly<GovBRDateTimePickerProps>) {
-
+export function GovBRDateTimePicker({ type, label }: Readonly<GovBRDateTimePickerProps>) {
   const [value, setValue] = React.useState<Dayjs | null>(dayjs)
 
   dayjs.locale('pt-br')
@@ -17,11 +13,16 @@ export function GovBRDateTimePicker ({
   const PickerComponent = pickerMap[type]
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="pt-br">
-    <PickerComponent 
-    label={label}
-    value={value}
-    onChange={(newValue: Dayjs) => setValue(newValue)}
-    />
+      <PickerComponent
+        label={label}
+        value={value}
+        onChange={(newValue: Dayjs) => setValue(newValue)}
+        slotProps={{
+          '& .MuiInputLabel-root': {
+            color: 'warning.main',
+          },
+        }}
+      />
     </LocalizationProvider>
   )
 }
