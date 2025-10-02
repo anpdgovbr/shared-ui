645370c42abf0243e54e2acd407fe5dd9a27bb82
eduHanjos0411 <eduardo.h.anjos0411@gmail.com>
2025-10-02 13:15:31 -0300

    feat: add GovBRTabs component with accessibility features and tab management

---
+++ src/components/ui/govbr-tabs/types.ts
@@ -0,0 +1,28 @@
+import { SxProps } from '@mui/material/styles'
+
+interface TabProps {
+  id: number
+  label?: string
+  icon?: React.ReactElement
+}
+
+interface TabsProps {
+  tabs: TabProps[]
+  children?: React.ReactNode[]
+  variant: 'text' | 'contained'
+  sx?: SxProps
+}
+
+interface GovBRTabsBaseProps extends TabsProps {
+  strictgovbr?: boolean
+}
+
+export type GovBRTabsStrictProps = GovBRTabsBaseProps & {
+  strictgovbr: true
+}
+
+export type GovBRTabsMuiProps = GovBRTabsBaseProps & {
+  strictgovbr: false
+}
+
+export type GovBRTabsProps = GovBRTabsStrictProps | GovBRTabsMuiProps
